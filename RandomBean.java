7f2bfa5e560430a2c984b5f0a278dc5089d82b34
Hamrouniryhem <102437660+Hamrouniryhem@users.noreply.github.com>
2022-05-20 02:24:44 +0200

    Add files via upload

---
+++ RandomBean.java
@@ -0,0 +1,20 @@
+package Beans;
+ public class RandomBean {
+	private double nb;
+	 
+	
+	public double getnb(){
+		return nb;
+	}
+	public void setnb(double nb){
+		this.nb=nb;
+	}
+	public RandomBean(){
+		setnb(0);
+	}
+	
+	public double calcule(double nb){
+		 return Math.random()*nb;
+	
+	}
+}
